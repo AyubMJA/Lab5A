61ae1e49dbb043ceb9e86f9af44e084f29cac37a
Ayub Ali <48361671+ayubali9996@users.noreply.github.com>
2021-08-13 13:47:25 -0400

    Faucet running now. Although placement doesn't fit where the faucet image exists for now.

--- Lab5A/Form1.cs
+++ Lab5A/Form1.cs
@@ -44,10 +44,7 @@ namespace Lab5A
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics bucket = CreateGraphics();
-            Pen linePen = new Pen(Color.White, 4);
-            bucket.DrawLine(linePen, 120, 650, 480, 650); //(x1,y1)(x2,y2) bottom piece
-            bucket.DrawLine(linePen, 120, 650, 120, 500); //(x1,y1)(x2,y2) left piece
-            bucket.DrawLine(linePen, 480, 650, 480, 500);//(x1,y1)(x2,y2) right piece
+            BucketDrawing(bucket);
         }
 
         /// <summary>
@@ -57,6 +54,22 @@ namespace Lab5A
         /// <param name="e"></param>
         private void colour_btn_Click(object sender, EventArgs e)
         {
+            DialogResult colorDialog = colorDialog1.ShowDialog();
+            if (colorDialog == DialogResult.OK)
+            {
+                currentColor = colorDialog1.Color;
+
+                if (trackBar1.Value > 0)
+                {
+                    currentColor = colorDialog1.Color;
+                    //Not finished obviously add some nonsense later.
+                }
+                else
+                {
+                    currentColor = colorDialog1.Color;
+                }
+            }
+
 
         }
 
@@ -77,12 +90,181 @@ namespace Lab5A
         /// <param name="e"></param>
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
-
+            if(trackBar1.Value == 0)
+            {
+                paint(Color.Black, faucetX1,faucetY1,faucetX2,faucetY2);
+                timer1.Stop();
+            }
+            else if(trackBar1.Value == 1)
+            {
+                if(bucketIsFull)
+                {
+                    bucketIsFull = false;
+                    Graphics black = CreateGraphics();
+                    Brush brushBlack = new SolidBrush(Color.Black);
+                    black.FillRectangle(brushBlack,66,250,213,100);
+                }
+                paint(colorDialog1.Color,faucetX1,faucetY1,faucetX2,faucetY2);
+                timer1.Interval = 500;
+                timer1.Start();
+            }
+            else if(trackBar1.Value == 2)
+            {
+                if(bucketIsFull)
+                {
+                    bucketIsFull = false;
+                    Graphics black = CreateGraphics();
+                    Brush brushBlack = new SolidBrush(Color.Black);
+                    black.FillRectangle(brushBlack,faucetX1,faucetY1,faucetX2,faucetY2);
+                }
+                paint(colorDialog1.Color,faucetX1,faucetY1,faucetX2,faucetY2);
+                timer1.Interval = 400;
+                timer1.Start();
+            }
+            else if (trackBar1.Value == 3)
+            {
+                if (bucketIsFull)
+                {
+                    bucketIsFull = false;
+                    Graphics black = CreateGraphics();
+                    Brush brushBlack = new SolidBrush(Color.Black);
+                    black.FillRectangle(brushBlack, faucetX1, faucetY1, faucetX2, faucetY2);
+                }
+                paint(colorDialog1.Color, faucetX1, faucetY1, faucetX2, faucetY2);
+                timer1.Interval = 300;
+                timer1.Start();
+            }
+            else if (trackBar1.Value == 4)
+            {
+                if (bucketIsFull)
+                {
+                    bucketIsFull = false;
+                    Graphics black = CreateGraphics();
+                    Brush brushBlack = new SolidBrush(Color.Black);
+                    black.FillRectangle(brushBlack, faucetX1, faucetY1, faucetX2, faucetY2);
+                }
+                paint(colorDialog1.Color, faucetX1, faucetY1, faucetX2, faucetY2);
+                timer1.Interval = 200;
+                timer1.Start();
+            }
+            else if (trackBar1.Value == 5)
+            {
+                if (bucketIsFull)
+                {
+                    bucketIsFull = false;
+                    Graphics black = CreateGraphics();
+                    Brush brushBlack = new SolidBrush(Color.Black);
+                    black.FillRectangle(brushBlack, faucetX1, faucetY1, faucetX2, faucetY2);
+                }
+                paint(colorDialog1.Color, faucetX1, faucetY1, faucetX2, faucetY2);
+                timer1.Interval = 100;
+                timer1.Start();
+            }
+            else if (trackBar1.Value == 6)
+            {
+                if (bucketIsFull)
+                {
+                    bucketIsFull = false;
+                    Graphics black = CreateGraphics();
+                    Brush brushBlack = new SolidBrush(Color.Black);
+                    black.FillRectangle(brushBlack, faucetX1, faucetY1, faucetX2, faucetY2);
+                }
+                paint(colorDialog1.Color, faucetX1, faucetY1, faucetX2, faucetY2);
+                timer1.Interval = 80;
+                timer1.Start();
+            }
+            else if (trackBar1.Value == 7)
+            {
+                if (bucketIsFull)
+                {
+                    bucketIsFull = false;
+                    Graphics black = CreateGraphics();
+                    Brush brushBlack = new SolidBrush(Color.Black);
+                    black.FillRectangle(brushBlack, faucetX1, faucetY1, faucetX2, faucetY2);
+                }
+                paint(colorDialog1.Color, faucetX1, faucetY1, faucetX2, faucetY2);
+                timer1.Interval = 60;
+                timer1.Start();
+            }
+            else if (trackBar1.Value == 8)
+            {
+                if (bucketIsFull)
+                {
+                    bucketIsFull = false;
+                    Graphics black = CreateGraphics();
+                    Brush brushBlack = new SolidBrush(Color.Black);
+                    black.FillRectangle(brushBlack, faucetX1, faucetY1, faucetX2, faucetY2);
+                }
+                paint(colorDialog1.Color, faucetX1, faucetY1, faucetX2, faucetY2);
+                timer1.Interval = 40;
+                timer1.Start();
+            }
+            else if (trackBar1.Value == 9)
+            {
+                if (bucketIsFull)
+                {
+                    bucketIsFull = false;
+                    Graphics black = CreateGraphics();
+                    Brush brushBlack = new SolidBrush(Color.Black);
+                    black.FillRectangle(brushBlack, faucetX1, faucetY1, faucetX2, faucetY2);
+                }
+                paint(colorDialog1.Color, faucetX1, faucetY1, faucetX2, faucetY2);
+                timer1.Interval = 20;
+                timer1.Start();
+            }
+            else if (trackBar1.Value == 10)
+            {
+                if (bucketIsFull)
+                {
+                    bucketIsFull = false;
+                    Graphics black = CreateGraphics();
+                    Brush brushBlack = new SolidBrush(Color.Black);
+                    black.FillRectangle(brushBlack, faucetX1, faucetY1, faucetX2, faucetY2);
+                }
+                paint(colorDialog1.Color, faucetX1, faucetY1, faucetX2, faucetY2);
+                timer1.Interval = 10;
+                timer1.Start();
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+
+        /// <summary>
+        /// Bucket placement on window Form
+        /// </summary>
+        /// <param name="bucket"></param>
+        public static void BucketDrawing(Graphics bucket)
+        {
+            Pen linePen = new Pen(Color.White, 4);
+            bucket.DrawLine(linePen, 120, 650, 480, 650); //(x1,y1)(x2,y2) bottom piece
+            bucket.DrawLine(linePen, 120, 650, 120, 500); //(x1,y1)(x2,y2) left piece
+            bucket.DrawLine(linePen, 480, 650, 480, 500);//(x1,y1)(x2,y2) right piece
+        }
+
+        /// <summary>
+        /// This is a function that handles the Faucet Paint color
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+       public void paint(Color color, int x1, int y1, int x2, int y2 )
+        {
+            faucetX1 = x1;
+            faucetY1 = y1;
+            faucetX2 = x2;
+            faucetY2 = y2;
+
+            currentColor = color;
+            Graphics graphics = CreateGraphics();
+            Pen pen = new Pen(color, 12);
+
+            graphics.DrawLine(pen, faucetX1, faucetY1, faucetX2, faucetY2);
         }
     }
 }
